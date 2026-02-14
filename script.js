e5d73cd44920a5b9eb44135b4e0eceece7572fa2
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-02-14 23:41:51 +0530

    run

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
+// SIGNUP FUNCTION
+function signup() {
+
+    let user = document.getElementById("newUser").value;
+    let pass = document.getElementById("newPass").value;
+
+    if (user === "" || pass === "") {
+        document.getElementById("msg").innerText = "Fill all fields ❌";
+        return;
+    }
+
+    // Save in localStorage
+    localStorage.setItem("username", user);
+    localStorage.setItem("password", pass);
+
+    document.getElementById("msg").style.color = "green";
+    document.getElementById("msg").innerText = "Signup Successful ✅";
+
+    // Go to login page after 1 sec
+    setTimeout(() => {
+        window.location.href = "login.html";
+    }, 1000);
+}
+
+
+
+// LOGIN FUNCTION
 function login() {
 
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    // Demo login credentials
-    let correctUser = "divya";
-    let correctPass = "1234";
+    // Get stored data
+    let storedUser = localStorage.getItem("username");
+    let storedPass = localStorage.getItem("password");
 
-    if (username === correctUser && password === correctPass) {
-        // Redirect to home page
+    if (username === storedUser && password === storedPass) {
+
         window.location.href = "home.html";
 
     } else {
 
-        document.getElementById("message").style.color = "red";
-        document.getElementById("message").innerText = "Invalid Username or Password ❌";
+        document.getElementById("message").innerText =
+            "Wrong Username or Password ❌";
     }
 }
